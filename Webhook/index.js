3f3d2fe2ea5be3f54ee0f00c69842ba4ae999825
Gabor Csizmadia <gaborcsiz@gmail.com>
2020-04-04 01:20:07 +0200

    Prevent duplicate responses

--- Webhook/index.js
+++ Webhook/index.js
@@ -4,6 +4,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const WEBHOOK_VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN;
 
 module.exports = async function (context, req) {
+    context.log(`req.rawBody: ${req.rawBody}`);
     if (req.method === "GET") {
         let isSubscribe = req.query["hub.mode"] === "subscribe";
         let isTokenCorrect = req.query["hub.verify_token"] === WEBHOOK_VERIFY_TOKEN;
@@ -20,13 +21,27 @@ module.exports = async function (context, req) {
         }
     } else if (req.method === "POST") {
         let client = df.getClient(context);
-        let messagingItems = req.body.entry.flatMap(e => e.messaging);
-        await Promise.all(messagingItems.map(messagingItem => {
-            return client.startNew("MessageHandler", undefined, {
-                messagingItem,
-                pageAccessToken: PAGE_ACCESS_TOKEN
-            });
+        let instanceIds = await Promise.all(req.body.entry.flatMap(entry => {
+            return entry.messaging.map(async messagingItem => {
+                let instanceId = `${entry.time}-${messagingItem.sender.id}`;
+                let existingInstance = await client.getStatus(instanceId);
+                let isRetriedWebhookCall = !!existingInstance;
+                if (!isRetriedWebhookCall) {
+                    // There is race condition here.
+                    // If the original webhook call's and a retried webhook call's processing reach
+                    // this point at the same time, then both will start the orchestration.
+                    // However, the chances of that are low enough, since FB only retries the call
+                    // every few seconds.
+                    return client.startNew("MessageHandler", instanceId, {
+                        messagingItem,
+                        pageAccessToken: PAGE_ACCESS_TOKEN
+                    });
+                } else {
+                    return instanceId;
+                }
+            })
         }));
+        context.log("MessageHandler instances started: " + instanceIds.join(", "));
         return {
             status: 200,
             body: "EVENT_RECEIVED"
