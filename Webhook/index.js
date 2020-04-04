dcfd49f204b6bffafeaa86ae1ab521ef3a013144
Gabor Csizmadia <gaborcsiz@gmail.com>
2020-04-04 11:12:41 +0200

    Minor clarification

--- Webhook/index.js
+++ Webhook/index.js
@@ -24,9 +24,13 @@ module.exports = async function (context, req) {
         let instanceIds = await Promise.all(req.body.entry.flatMap(entry => {
             return entry.messaging.map(async messagingItem => {
                 let instanceId = `${entry.time}-${messagingItem.sender.id}`;
+                // The player should be able to send only a single message/postback at a time.
+                // If an instance with this instanceId already exists, this is probably a retried
+                // webhook call, which we shouldn't handle.
                 let existingInstance = await client.getStatus(instanceId);
-                let isRetriedWebhookCall = !!existingInstance;
-                if (!isRetriedWebhookCall) {
+                if (existingInstance) {
+                    return instanceId;
+                } else {
                     // There is race condition here.
                     // If the original webhook call's and a retried webhook call's processing reach
                     // this point at the same time, then both will start the orchestration.
@@ -36,8 +40,6 @@ module.exports = async function (context, req) {
                         messagingItem,
                         pageAccessToken: PAGE_ACCESS_TOKEN
                     });
-                } else {
-                    return instanceId;
                 }
             })
         }));
